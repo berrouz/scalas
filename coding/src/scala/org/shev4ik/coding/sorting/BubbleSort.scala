947ec6625ac88c6614ec23c12c2e325293766b75
Sergii Shevchyk <berrouz@gmail.com>
2019-05-11 22:06:41 +0300

    add bubble sort

--- coding/src/scala/org/shev4ik/coding/sorting/BubbleSort.scala
+++ coding/src/scala/org/shev4ik/coding/sorting/BubbleSort.scala
@@ -1,5 +1,20 @@
 package scala.org.shev4ik.coding.sorting
 
 object BubbleSort extends App {
+  val arr = Array(3, 7, 9, 8, 4, 5, 1, 6, 2, 0)
 
+  sort(arr) foreach print
+
+  def sort(arr: Array[Int]): Array[Int] = {
+    for (i <- arr.indices) {
+      for (j <- 0 until i) {
+        if (arr(i) < arr(j)) {
+          val temp = arr(i)
+          arr(i) = arr(j)
+          arr(j) = temp
+        }
+      }
+    }
+    arr
+  }
 }
