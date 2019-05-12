8e31272b393e8d43ef056012e3a34ea3149a1c5c
Sergii Shevchyk <berrouz@gmail.com>
2019-05-12 08:48:51 +0300

    add last but one with recursion

---
+++ coding/src/scala/org/shev4ik/coding/linkedlist/FindLastButOneElement.scala
@@ -0,0 +1,16 @@
+package org.shev4ik.coding.linkedlist
+
+object FindLastButOneElement extends App {
+
+  Some(lastButOne(1 :: 1 :: 2 :: 3 :: 5 :: 8 :: Nil)) foreach println
+
+  def lastButOne(list: List[Int]): Int = {
+    def inner(list: List[Int]): Int = {
+      list match {
+        case prev :: _ :: Nil ⇒ prev
+        case _ :: tail ⇒ inner(tail)
+      }
+    }
+    inner(list)
+  }
+}
