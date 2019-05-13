8c949de455b98c15da896be0f1db2af90740d617
Sergii Shevchyk <berrouz@gmail.com>
2019-05-13 12:56:05 +0300

    add simple akka example

--- coding/build.sbt
+++ coding/build.sbt
@@ -3,5 +3,7 @@ import scala.language.postfixOps
 name := "coding"
 
 libraryDependencies ++= {
-  Seq("org.scalatest" %% "scalatest" % "3.0.6")
+  Seq(
+    "org.scalatest" %% "scalatest" % "3.0.6",
+    "com.typesafe.akka" %% "akka-actor" % "2.5.22")
 }
---
+++ coding/src/main/scala/org/shev4ik/coding/AkkaSystemApp.scala
@@ -0,0 +1,43 @@
+package org.shev4ik.coding
+
+import java.util.Date
+
+import akka.actor.Actor
+import akka.actor.ActorRef
+import akka.actor.ActorSystem
+import akka.actor.Props
+import akka.util.Timeout
+
+import scala.concurrent.duration._
+object AkkaSystemApp extends App {
+  implicit val system: ActorSystem = ActorSystem()
+
+  val actor = system.actorOf(SuperActor.props)
+  implicit val timeout: Timeout = 2.seconds
+  actor ! Start
+}
+
+class SuperActor extends Actor {
+  val dateActor: ActorRef = context.actorOf(Props[CurrentDateActor])
+  override def receive: Receive = {
+    case Start ⇒ dateActor ! GetDate
+    case CurrentDate(date) ⇒ println(s"Current date is $date")
+  }
+}
+object SuperActor {
+  def props: Props = Props(new SuperActor())
+}
+
+class CurrentDateActor extends Actor {
+  override def receive: Receive = {
+    case GetDate ⇒ sender() ! CurrentDate(new Date())
+  }
+}
+
+object CurrentDateActor {
+  def props: Props = Props(new CurrentDateActor)
+}
+
+object GetDate
+object Start
+case class CurrentDate(date: Date)
